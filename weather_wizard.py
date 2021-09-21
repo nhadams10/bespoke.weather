ac9d58f956193771107df31bf06330bf4bda5658
Noah Adams <nhadams10@uchicago.edu>
2021-09-21 12:03:09 +0000

    Started building

---
+++ weather_wizard.py
@@ -0,0 +1,17 @@
+commute_time = input("How long is your commute? (in minutes) ")
+
+print(
+'''
+ ––––––––––––––––––––––––––––––––––––––––––
+|Enter the letter for your means of transit|
+| - - - - - - - - - - - - - - - - - - - - -|
+|w - walk                                  |
+|b - bicycle                               |
+|p - public transit                        |
+|c - car                                   |
+ ––––––––––––––––––––––––––––––––––––––––––
+'''
+)
+commute_method = input("How are you getting to your destination? ")
+location = "Seoul"
+weather = "Good"
